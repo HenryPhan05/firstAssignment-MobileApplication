2679936a6073fdfb76d8eaf425667fec8d92dd8a
Cielo Pacot <cielopacot@Cielos-Laptop.local>
2026-02-13 21:52:22 -0700

    Initial To Do App Assignment

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+/**
+ * My To Do List App
+ *
+ * @format
+ */
 
-export default function Index() {
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+function App(): JSX.Element {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Back to see the spotify-clone</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
   );
 }
+
+export default App;
